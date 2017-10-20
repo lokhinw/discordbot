b7fc121c9b60f13502a03e60509c7bbec2833d01
Lokhin Wu <lokhin.wu@live.com>
2017-10-19 21:03:47 -0400

    8 Ball

--- app.js
+++ app.js
@@ -32,6 +32,16 @@ client.on('message', message => {
         console.log(err);
       }
       break;
+    case config.prefix + '8ball':
+      var eightBallAnswers = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes, definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Don\'t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful'];
+      let eightBallEmbed = new Discord.RichEmbed()
+      if (args.length !== 0) {
+        eightBallEmbed.setDescription(':8ball: ' + eightBallAnswers[[Math.floor(Math.random() * eightBallAnswers.length)]]);
+      } else {
+        eightBallEmbed.setDescription(':8ball: ' + ' You must ask me something!');
+      }
+      message.channel.sendEmbed(eightBallEmbed);
+      break;
   }
 });
 
